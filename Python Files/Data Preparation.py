90ab0d922ebd59824f7d7cfa0b2dc2ed544e39e3
Danté G <16039915+DarkMasquerader@users.noreply.github.com>
2024-05-04 12:33:49 +0100

    Create Data Preparation.py

---
+++ Python Files/Data Preparation.py
@@ -0,0 +1,12 @@
+'''
+This is the Python script used for cleaning the data.
+'''
+import pandas as pd
+import numpy as np
+
+def main():
+    pass
+    
+# Main Loop
+if __name__ == '__main__':
+    main()
