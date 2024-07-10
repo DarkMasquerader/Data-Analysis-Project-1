93c1e7f003a97b0592ce2f66b387f8dabdd505af
Danté G <16039915+DarkMasquerader@users.noreply.github.com>
2024-07-10 13:54:27 +0100

    Update MyGameClass.py

--- Python Files/Data Acquisition/MyGameClass.py
+++ Python Files/Data Acquisition/MyGameClass.py
@@ -47,9 +47,24 @@ class Game:
         return self.tag_set
 
     '''
-    Returns a nested list with each row containing the x-th value from each of the lists.
-    Structure: Title, Rank, Month, Average Peak
-    NOTE: The return value needs to be iterated by row to be passed into the dataframe
+        Description
+        ------------
+        Returns a nested list with each row containing the x-th value from each of the lists [Structure: Title, Rank, Month, Average Peak]
+
+        NOTE: The return value needs to be iterated by row to be passed into the DataFrame object.
+        
+        Parameters
+        ----------
+        This function does not take any parameters.
+        
+        Returns
+        -------
+        self.return_list: list
+            A list containing containing the row-values (Title, Rank, Month, Average Peak) for each game.
+        
+        Raises
+        ------
+        This function does not raise any exceptions.
     '''
     def to_stats_list(self):
         self.return_list = []
@@ -58,6 +73,25 @@ class Game:
         
         return self.return_list
     
+    '''
+        Description
+        ------------
+        This function converts the scraped date into a format that can be handled by Tableau.
+    
+        Parameters
+        ----------
+        date: str
+            This is the text representation fo the date scraped from SteamCharts.
+    
+        Returns
+        -------
+        _: str
+            A textual representation of the date in a format that can be processed by Tableau.
+    
+        Raises
+        ------
+        This function does not raise any exceptions.
+    '''
     month_to_number = {
         'January': 1,
         'February': 2,
@@ -76,12 +110,27 @@ class Game:
         split_date = date.split(' ')
         
         if len(split_date) == 3: # Value: Last 30 Days
-            return '5-2024'
+            return '5-2024' #Naughty hard-coded value
         else:
             return f'{Game.month_to_number[split_date[0]]}-{split_date[1]}'
 
     '''
-    Returns a list of details for the dataframe showing the information of the game stored on Steam's website
+        Description
+        ------------
+        Returns a list of details for the DataFrame object which contains the information available for a game on the Steam website.
+    
+        Parameters
+        ----------
+        This function does not take any parameters.
+    
+        Returns
+        -------
+        _: list
+            A list containing the details scraped from Steam.
+    
+        Raises
+        ------
+        This function does not raise any exceptions.
     '''
     def to_details(self):
         if self.price is None or '£' not in self.price:
